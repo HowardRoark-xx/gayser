48f97e03fc0519674344ab8b086769955ea5905c
Amar <amarnath.mohan@gmail.com>
2023-05-12 18:35:54 +0530

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,1 +1,4 @@
-R -e 'shiny::runApp("./app.R", port=8888, host="0.0.0.0")'
+#R -e 'shiny::runApp("./app.R", port=8888, host="0.0.0.0")'
+pip3 install streamlit
+
+streamlit run --server.port 8888 app.py
